3314ce06d3e0c4902203bfe4c003df940c2fa160
vishal-joshy <1812-vishal@protonmail.com>
2023-12-05 10:23:54 +0530

    Add support for cmd parameters

--- add_task.sh
+++ add_task.sh
@@ -1,7 +1,10 @@
+if [[ $# -eq 4 ]]; then
+	echo "$(date +'%d/%m/%Y'),$1,$2,$3,$4" >>./task.csv
+else
+	read -p "Task: " task
+	read -p "Task Description: " taskdesc
+	read -p "Business function: " bfunc
+	read -p "Hours: " hours
 
-read -p "Task: " task
-read -p "Task Description: " taskdesc
-read -p "Business function: " bfunc
-read -p "Hours: " hours
-
-echo "$(date +'%d/%m/%Y'),$task,$bfunc,$taskdesc,$hours" >> ./task.csv
+	echo "$(date +'%d/%m/%Y'),$task,$bfunc,$taskdesc,$hours" >>./task.csv
+fi
